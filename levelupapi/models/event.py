55041075c347e24c6a06faefe14a599902e0d2a8
justha <nganha42@gmail.com>
2020-11-06 11:53:30 -0600

    fixed games and events ORM

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -4,9 +4,8 @@ from django.db import models
 
 class Event(models.Model):
     """Event database model"""
+    organizer = models.ForeignKey("Gamer", on_delete=models.CASCADE, related_name='events')
+    description = models.CharField(max_length=50)
     game = models.ForeignKey("Game", on_delete=models.CASCADE, related_name='events')
-    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE, related_name='events')
-    day = models.DateField(auto_now=False, auto_now_add=False)
+    date = models.DateField(auto_now=False, auto_now_add=False)
     time = models.TimeField(auto_now=False, auto_now_add=False)
-    location = models.CharField(max_length=50)
-    
