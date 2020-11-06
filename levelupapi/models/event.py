1420acbe8580ecc38819c253bb3bf2aeb2e67889
justha <nganha42@gmail.com>
2020-11-06 10:15:43 -0600

    reg and login auth; data views and urls created for gametypes, games, events

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Event(models.Model):
     """Event database model"""
-    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
-    game = models.ForeignKey("Game", on_delete=models.CASCADE)
+    game = models.ForeignKey("Game", on_delete=models.CASCADE, related_name='events')
+    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE, related_name='events')
     day = models.DateField(auto_now=False, auto_now_add=False)
     time = models.TimeField(auto_now=False, auto_now_add=False)
     location = models.CharField(max_length=50)
